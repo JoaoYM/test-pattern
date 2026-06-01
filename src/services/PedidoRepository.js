e533823994d27a94348b96f1776c5eff348748fa
joao_aguiar <aguiar.prado12@gmail.com>
2026-05-31 23:46:05 -0300

    refactor: converte modulos ES6 para CommonJS para compatibilidade com Jest

--- src/services/PedidoRepository.js
+++ src/services/PedidoRepository.js
@@ -1,6 +1,8 @@
-export class PedidoRepository {
+class PedidoRepository {
     // Retorna o pedido salvo (com ID)
     async salvar(pedido) {
         throw new Error("Não deve salvar no DB real");
     }
 }
+
+module.exports = { PedidoRepository };
